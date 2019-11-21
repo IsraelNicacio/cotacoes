c5e56bb33022e1e8c925620f8e3a4e226c0bb691
Israel <israel.nicacio@outlook.com>
2019-11-21 11:07:14 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -71,7 +71,7 @@ app.get('*', (req, res) => {
     });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port;
 
 app.listen(port, () => {
     console.log(`server is up port ${port}`);
